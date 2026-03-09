15d4f5ae807b5db196826dcbf228bc6f32c9d04e
Admin <ida.tv@bk.ru>
2026-03-09 18:46:59 +0200

    add menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,3 +16,47 @@ export const metadata = {
     type: "website",
   },
 }
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+
+      <body>
+
+        {/* МЕНЮ */}
+        <nav style={{
+          display:"flex",
+          gap:"20px",
+          padding:"20px",
+          background:"#000",
+          color:"#fff"
+        }}>
+
+          <a href="/order">Главная</a>
+
+          <a href="https://www.ida-tv.eu/Наши-услуги-1" target="_blank">
+            Услуги
+          </a>
+
+          <a href="/order">
+            Подключение
+          </a>
+
+          <a href="/feedback">
+            Обратная связь
+          </a>
+
+          <a href="/contacts">
+            Контакты
+          </a>
+
+        </nav>
+
+        {/* СТРАНИЦЫ */}
+        {children}
+
+      </body>
+
+    </html>
+  )
+}
