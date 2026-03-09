db821e88219a7c4f769588b526a5fda4f0fb45e6
Admin <ida.tv@bk.ru>
2026-03-09 19:11:23 +0200

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,3 +16,13 @@ export const metadata = {
     type: "website",
   },
 }
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
