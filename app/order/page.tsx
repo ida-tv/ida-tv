778bcccd863d9590575682ee7550fc6c1e38c9e6
Admin <ida.tv@bk.ru>
2026-03-10 20:17:00 +0200

    remove prisma config

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -131,7 +131,7 @@ export default function OrderPage() {
             <option>Подключение на месте — 70–125€</option>
             <option>Удалённая настройка — 65€</option>
             <option>Аренда оборудования — 15€/мес</option>
-            <option>Android TV Box_YOKATV -100 - 125€</option>
+            <option>Купить_Android TV Box_YOKATV -100 - 125€</option>
             <option>Вызов техника — 10€</option>
           </select>
 
